5139772627a75dcca1b58107f814e46d84b55882
Tobias Koppers <tobias.koppers@googlemail.com>
2012-08-13 17:07:37 +0200

    tests

--- test/simple.js
+++ test/simple.js
@@ -3,19 +3,20 @@ var should = require("should");
 var path = require("path");
 
 describe("simple", function() {
-	it("should resolve itself", function(done) {
-		var pathsToIt = [
-			[__dirname, "../lib/resolve"],
-			[__dirname, "../"],
-			[path.join(__dirname, "..", "..", ".."), "enhanced-require"],
-			[path.join(__dirname, "..", "..", ".."), "enhanced-require/lib/resolve"]
-		];
-		pathsToIt.forEach(function(pathToIt) {
+	var pathsToIt = [
+		[__dirname, "../lib/resolve", "direct"],
+		[__dirname, "../", "as directory"],
+		[path.join(__dirname, "..", "..", ".."), "enhanced-resolve", "as module"],
+		[path.join(__dirname, "..", "..", ".."), "enhanced-resolve/lib/resolve", "in module"]
+	];
+	pathsToIt.forEach(function(pathToIt) {
+		it("should resolve itself " + pathToIt[2], function(done) {
 			resolve(pathToIt[0], pathToIt[1], function(err, filename) {
 				if(err) throw err;
 				should.exist(filename);
 				filename.should.be.a("string");
 				filename.should.be.eql(path.join(__dirname, "..", "lib", "resolve.js"));
+				done();
 			});
 		});
 	});
