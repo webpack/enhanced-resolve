ce7ef6cb9a92e1d97441ae8ae24b247ef8f2c162
Tobias Koppers <tobias.koppers@googlemail.com>
2015-12-26 23:27:51 +0100

    added context parameter to resolve methods

--- test/alias.js
+++ test/alias.js
@@ -63,34 +63,34 @@ describe("alias", function() {
 	});
 
 	it("should resolve a not aliased module", function() {
-		resolver.resolveSync("/", "a").should.be.eql("/a/index");
-		resolver.resolveSync("/", "a/index").should.be.eql("/a/index");
-		resolver.resolveSync("/", "a/dir").should.be.eql("/a/dir/index");
-		resolver.resolveSync("/", "a/dir/index").should.be.eql("/a/dir/index");
+		resolver.resolveSync({}, "/", "a").should.be.eql("/a/index");
+		resolver.resolveSync({}, "/", "a/index").should.be.eql("/a/index");
+		resolver.resolveSync({}, "/", "a/dir").should.be.eql("/a/dir/index");
+		resolver.resolveSync({}, "/", "a/dir/index").should.be.eql("/a/dir/index");
 	});
 	it("should resolve an aliased module", function() {
-		resolver.resolveSync("/", "aliasA").should.be.eql("/a/index");
-		resolver.resolveSync("/", "aliasA/index").should.be.eql("/a/index");
-		resolver.resolveSync("/", "aliasA/dir").should.be.eql("/a/dir/index");
-		resolver.resolveSync("/", "aliasA/dir/index").should.be.eql("/a/dir/index");
+		resolver.resolveSync({}, "/", "aliasA").should.be.eql("/a/index");
+		resolver.resolveSync({}, "/", "aliasA/index").should.be.eql("/a/index");
+		resolver.resolveSync({}, "/", "aliasA/dir").should.be.eql("/a/dir/index");
+		resolver.resolveSync({}, "/", "aliasA/dir/index").should.be.eql("/a/dir/index");
 	});
 	it("should resolve a recursive aliased module", function() {
-		resolver.resolveSync("/", "recursive").should.be.eql("/recursive/dir/index");
-		resolver.resolveSync("/", "recursive/index").should.be.eql("/recursive/dir/index");
-		resolver.resolveSync("/", "recursive/dir").should.be.eql("/recursive/dir/index");
-		resolver.resolveSync("/", "recursive/dir/index").should.be.eql("/recursive/dir/index");
+		resolver.resolveSync({}, "/", "recursive").should.be.eql("/recursive/dir/index");
+		resolver.resolveSync({}, "/", "recursive/index").should.be.eql("/recursive/dir/index");
+		resolver.resolveSync({}, "/", "recursive/dir").should.be.eql("/recursive/dir/index");
+		resolver.resolveSync({}, "/", "recursive/dir/index").should.be.eql("/recursive/dir/index");
 	});
 	it("should resolve a file aliased module", function() {
-		resolver.resolveSync("/", "b").should.be.eql("/a/index");
-		resolver.resolveSync("/", "c").should.be.eql("/a/index");
+		resolver.resolveSync({}, "/", "b").should.be.eql("/a/index");
+		resolver.resolveSync({}, "/", "c").should.be.eql("/a/index");
 	});
 	it("should resolve a path in a file aliased module", function() {
-		resolver.resolveSync("/", "b/index").should.be.eql("/b/index");
-		resolver.resolveSync("/", "b/dir").should.be.eql("/b/dir/index");
-		resolver.resolveSync("/", "b/dir/index").should.be.eql("/b/dir/index");
-		resolver.resolveSync("/", "c/index").should.be.eql("/c/index");
-		resolver.resolveSync("/", "c/dir").should.be.eql("/c/dir/index");
-		resolver.resolveSync("/", "c/dir/index").should.be.eql("/c/dir/index");
+		resolver.resolveSync({}, "/", "b/index").should.be.eql("/b/index");
+		resolver.resolveSync({}, "/", "b/dir").should.be.eql("/b/dir/index");
+		resolver.resolveSync({}, "/", "b/dir/index").should.be.eql("/b/dir/index");
+		resolver.resolveSync({}, "/", "c/index").should.be.eql("/c/index");
+		resolver.resolveSync({}, "/", "c/dir").should.be.eql("/c/dir/index");
+		resolver.resolveSync({}, "/", "c/dir/index").should.be.eql("/c/dir/index");
 	});
 
 });
--- test/browserField.js
+++ test/browserField.js
@@ -33,33 +33,33 @@ describe("browserField", function() {
 	});
 
 	it("should ignore", function(done) {
-		resolver.resolve(p(), "./lib/ignore", function(err, result) {
+		resolver.resolve({}, p(), "./lib/ignore", function(err, result) {
 			if(err) throw err;
 			result.should.be.eql(false);
 			done();
 		})
 	});
 	it("should ignore", function() {
-		resolver.resolveSync(p(), "./lib/ignore").should.be.eql(false);
-		resolver.resolveSync(p(), "./lib/ignore.js").should.be.eql(false);
-		resolver.resolveSync(p("lib"), "./ignore").should.be.eql(false);
-		resolver.resolveSync(p("lib"), "./ignore.js").should.be.eql(false);
+		resolver.resolveSync({}, p(), "./lib/ignore").should.be.eql(false);
+		resolver.resolveSync({}, p(), "./lib/ignore.js").should.be.eql(false);
+		resolver.resolveSync({}, p("lib"), "./ignore").should.be.eql(false);
+		resolver.resolveSync({}, p("lib"), "./ignore.js").should.be.eql(false);
 	});
 
 	it("should replace a file", function() {
-		resolver.resolveSync(p(), "./lib/replaced").should.be.eql(p("lib", "browser.js"));
-		resolver.resolveSync(p(), "./lib/replaced.js").should.be.eql(p("lib", "browser.js"));
-		resolver.resolveSync(p("lib"), "./replaced").should.be.eql(p("lib", "browser.js"));
-		resolver.resolveSync(p("lib"), "./replaced.js").should.be.eql(p("lib", "browser.js"));
+		resolver.resolveSync({}, p(), "./lib/replaced").should.be.eql(p("lib", "browser.js"));
+		resolver.resolveSync({}, p(), "./lib/replaced.js").should.be.eql(p("lib", "browser.js"));
+		resolver.resolveSync({}, p("lib"), "./replaced").should.be.eql(p("lib", "browser.js"));
+		resolver.resolveSync({}, p("lib"), "./replaced.js").should.be.eql(p("lib", "browser.js"));
 	});
 
 	it("should replace a module with a file", function() {
-		resolver.resolveSync(p(), "module-a").should.be.eql(p("browser", "module-a.js"));
-		resolver.resolveSync(p("lib"), "module-a").should.be.eql(p("browser", "module-a.js"));
+		resolver.resolveSync({}, p(), "module-a").should.be.eql(p("browser", "module-a.js"));
+		resolver.resolveSync({}, p("lib"), "module-a").should.be.eql(p("browser", "module-a.js"));
 	});
 
 	it("should replace a module with a module", function() {
-		resolver.resolveSync(p(), "module-b").should.be.eql(p("node_modules", "module-c.js"));
-		resolver.resolveSync(p("lib"), "module-b").should.be.eql(p("node_modules", "module-c.js"));
+		resolver.resolveSync({}, p(), "module-b").should.be.eql(p("node_modules", "module-c.js"));
+		resolver.resolveSync({}, p("lib"), "module-b").should.be.eql(p("node_modules", "module-c.js"));
 	});
 });
