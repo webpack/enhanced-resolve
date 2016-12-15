7e6ebb5efceede1bbd2166655300c1c6b7f03140
Tobias Koppers <tobias.koppers@googlemail.com>
2016-12-16 00:19:58 +0100

    add test case for sync resolving

---
+++ test/pr-53.js
@@ -0,0 +1,13 @@
+var should = require("should");
+var CachedInputFileSystem = require("../lib/CachedInputFileSystem");
+
+describe("pr-53", function() {
+	it("should allow to readJsonSync in CachedInputFileSystem", function() {
+		var cfs = new CachedInputFileSystem({
+			readFileSync: function(path) {
+				return JSON.stringify("abc" + path);
+			}
+		}, 1000);
+		cfs.readJsonSync("xyz").should.be.eql("abcxyz");
+	});
+});
