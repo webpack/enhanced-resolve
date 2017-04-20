39544e6e031378a65bc5cc9e564b807b3505843e
Shahar Or (mightyiam) <mightyiampresence@gmail.com>
2017-04-20 21:51:15 +0300

    two tests for the extensions option

---
+++ test/extensions.js
@@ -0,0 +1,29 @@
+var path = require("path");
+require("should");
+var ResolverFactory = require("../lib/ResolverFactory");
+var NodeJsInputFileSystem = require("../lib/NodeJsInputFileSystem");
+var CachedInputFileSystem = require("../lib/CachedInputFileSystem");
+
+var nodeFileSystem = new CachedInputFileSystem(new NodeJsInputFileSystem(), 4000);
+
+var resolver = ResolverFactory.createResolver({
+	extensions: [".ts", ".js"],
+	fileSystem: nodeFileSystem
+});
+
+var fixture = path.resolve(__dirname, "fixtures", "extensions");
+
+describe("extensions", function() {
+	it("should resolve according to order of provided extensions", function(done) {
+		resolver.resolve({}, fixture, "./foo", function(err, result) {
+			result.should.equal(path.resolve(fixture, "foo.ts"));
+			done();
+		});
+	});
+	it("should resolve according to order of provided extensions (dir index)", function(done) {
+		resolver.resolve({}, fixture, ".", function(err, result) {
+			result.should.equal(path.resolve(fixture, "index.ts"));
+			done();
+		});
+	});
+});
