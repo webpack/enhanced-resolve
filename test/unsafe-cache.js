b03799b6f06f82423e0a5c57fcc196b8eaeb8c07
Mike Sherov <mike.sherov@gmail.com>
2017-06-27 18:36:38 -0400

    introduce aggressiveCache option that excludes `request.context` from unsafeCache cache keys

--- test/unsafe-cache.js
+++ test/unsafe-cache.js
@@ -2,46 +2,115 @@ var resolve = require("../");
 var path = require("path");
 
 describe("unsafe-cache", function() {
-	it("should cache request", function(done) {
+	var cache;
+	var cachedResolve;
+	var context;
+	var otherContext;
 
-		var cache = {};
-		var cachedResolve = resolve.create({
-			unsafeCache: cache
-		});
+	beforeEach(function() {
+		context = {
+			some: "context"
+		};
+		otherContext = {
+			someOther: "context"
+		};
+	});
 
-		cachedResolve(path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
-			if(err) return done(err);
-			Object.keys(cache).should.have.length(2);
-			Object.keys(cache).forEach(function(key) {
-				cache[key] = {
-					path: "yep"
-				};
+	describe("with no other options", function() {
+		beforeEach(function() {
+			cache = {};
+			cachedResolve = resolve.create({
+				unsafeCache: cache
 			});
+		});
+		it("should cache request", function(done) {
 			cachedResolve(path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
 				if(err) return done(err);
-				result.should.be.eql("yep");
-				done();
+				Object.keys(cache).should.have.length(2);
+				Object.keys(cache).forEach(function(key) {
+					cache[key] = {
+						path: "yep"
+					};
+				});
+				cachedResolve(path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
+					if(err) return done(err);
+					result.should.be.eql("yep");
+					done();
+				});
+			});
+		});
+		it("should not return from cache if context does not match", function(done) {
+			cachedResolve(context, path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
+				if(err) return done(err);
+				Object.keys(cache).should.have.length(2);
+				Object.keys(cache).forEach(function(key) {
+					cache[key] = {
+						path: "yep"
+					};
+				});
+				cachedResolve(otherContext, path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
+					if(err) return done(err);
+					result.should.not.be.eql("yep");
+					done();
+				});
+			});
+		});
+		it("should not return from cache if query does not match", function(done) {
+			cachedResolve(path.join(__dirname, "fixtures"), "m2/b?query", function(err, result) {
+				if(err) return done(err);
+				Object.keys(cache).should.have.length(2);
+				Object.keys(cache).forEach(function(key) {
+					cache[key] = {
+						path: "yep"
+					};
+				});
+				cachedResolve(path.join(__dirname, "fixtures"), "m2/b?query2", function(err, result) {
+					if(err) return done(err);
+					result.should.not.be.eql("yep");
+					done();
+				});
 			});
 		});
 	});
-	it("should not return from cache if query does not match", function(done) {
-		var cache = {};
-		var cachedResolve = resolve.create({
-			unsafeCache: cache
-		});
 
-		cachedResolve(path.join(__dirname, "fixtures"), "m2/b?query", function(err, result) {
-			if(err) return done(err);
-			Object.keys(cache).should.have.length(2);
-			Object.keys(cache).forEach(function(key) {
-				cache[key] = {
-					path: "yep"
-				};
+	describe("with cacheWithContext false", function() {
+		beforeEach(function() {
+			cache = {};
+			cachedResolve = resolve.create({
+				unsafeCache: cache,
+				cacheWithContext: false
 			});
-			cachedResolve(path.join(__dirname, "fixtures"), "m2/b?query2", function(err, result) {
+		});
+		it("should cache request", function(done) {
+			cachedResolve(context, path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
 				if(err) return done(err);
-				result.should.not.be.eql("yep");
-				done();
+				Object.keys(cache).should.have.length(2);
+				Object.keys(cache).forEach(function(key) {
+					cache[key] = {
+						path: "yep"
+					};
+				});
+				cachedResolve(context, path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
+					if(err) return done(err);
+					result.should.be.eql("yep");
+					done();
+				});
+			});
+		});
+		it("should return from cache even if context does not match", function(done) {
+			cachedResolve(context, path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
+				if(err) return done(err);
+				Object.keys(cache).should.have.length(2);
+				Object.keys(cache).forEach(function(key) {
+					cache[key] = {
+						path: "yep"
+					};
+				});
+				cachedResolve(otherContext, path.join(__dirname, "fixtures"), "m2/b", function(err, result) {
+					if(err) return done(err);
+					result.should.be.eql("yep");
+					done();
+				});
 			});
 		});
 	});
