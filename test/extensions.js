8d341981967da3962516b8ed5b89054f97ba4ddd
Tobias Koppers <tobias.koppers@googlemail.com>
2019-07-04 20:43:53 +0200

    prefer single file module over directory, optimizations

--- test/extensions.js
+++ test/extensions.js
@@ -15,20 +15,30 @@ const fixture = path.resolve(__dirname, "fixtures", "extensions");
 
 describe("extensions", function() {
 	it("should resolve according to order of provided extensions", function(done) {
-		resolver.resolve({}, fixture, "./foo", {}, function(err, result) {
+		resolver.resolve({}, fixture, "./foo", {}, (err, result) => {
+			if (err) return done(err);
 			result.should.equal(path.resolve(fixture, "foo.ts"));
 			done();
 		});
 	});
 	it("should resolve according to order of provided extensions (dir index)", function(done) {
-		resolver.resolve({}, fixture, "./dir", {}, function(err, result) {
+		resolver.resolve({}, fixture, "./dir", {}, (err, result) => {
+			if (err) return done(err);
 			result.should.equal(path.resolve(fixture, "dir", "index.ts"));
 			done();
 		});
 	});
 	it("should resolve according to main field in module root", function(done) {
-		resolver.resolve({}, fixture, ".", {}, function(err, result) {
+		resolver.resolve({}, fixture, ".", {}, (err, result) => {
+			if (err) return done(err);
 			result.should.equal(path.resolve(fixture, "index.js"));
+			done();
+		});
+	});
+	it("should resolve single file module before directory", function(done) {
+		resolver.resolve({}, fixture, "module", {}, (err, result) => {
+			if (err) return done(err);
+			result.should.equal(path.resolve(fixture, "node_modules/module.js"));
 			done();
 		});
 	});
