cf6e3a81d173c40276e4db2bd93d17f32a160713
Tobias Koppers <tobias.koppers@googlemail.com>
2020-03-04 15:31:49 +0100

    update dev deps and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,49 +15,61 @@ module.exports = {
 	},
 	rules: {
 		"prettier/prettier": "error",
-		"no-undef": "error",
-		"no-extra-semi": "error",
 		"no-template-curly-in-string": "error",
 		"no-caller": "error",
 		"no-control-regex": "off",
 		yoda: "error",
 		eqeqeq: "error",
-		"global-require": "off",
-		"brace-style": "off",
 		"eol-last": "error",
 		"no-extra-bind": "warn",
 		"no-process-exit": "warn",
 		"no-use-before-define": "off",
-		"no-unused-vars": ["error", { args: "none" }],
-		"no-unsafe-negation": "error",
+		"no-unused-vars": ["error", { args: "none", ignoreRestSiblings: true }],
 		"no-loop-func": "warn",
-		indent: "off",
-		"no-console": "off",
-		"valid-jsdoc": [
-			"error",
-			{
-				prefer: {
-					return: "returns",
-					prop: "property",
-					memberof: "DONTUSE",
-					class: "DONTUSE",
-					inheritdoc: "DONTUSE",
-					description: "DONTUSE",
-					readonly: "DONTUSE"
-				},
-				preferType: {
-					"*": "any"
-				},
-				requireReturnType: true
-			}
-		],
-		"node/no-unsupported-features": "error",
-		"node/no-deprecated-api": "error",
-		"node/no-missing-import": "error",
-		"node/no-unpublished-bin": "error",
-		"node/no-unpublished-require": "error",
-		"node/process-exit-as-throw": "error",
-		"jsdoc/require-hyphen-before-param-description": ["error", "never"]
+		"node/no-missing-require": ["error", { allowModules: ["webpack"] }],
+		"jsdoc/check-indentation": "error",
+		"jsdoc/check-param-names": "error",
+		"jsdoc/check-property-names": "error",
+		"jsdoc/check-tag-names": "error",
+		"jsdoc/require-hyphen-before-param-description": ["error", "never"],
+		"jsdoc/require-param-description": "error",
+		"jsdoc/require-param-name": "error",
+		"jsdoc/require-param-type": "error",
+		"jsdoc/require-param": "error",
+		"jsdoc/require-property": "error",
+		"jsdoc/require-property-name": "error",
+		"jsdoc/require-property-type": "error",
+		"jsdoc/require-returns-description": "error",
+		"jsdoc/require-returns-type": "error",
+		"jsdoc/require-returns": "error"
+	},
+	settings: {
+		jsdoc: {
+			mode: "typescript",
+			// supported tags https://github.com/microsoft/TypeScript-wiki/blob/master/JSDoc-support-in-JavaScript.md
+			tagNamePreference: {
+				...["implements", "const", "memberof", "readonly", "yields"].reduce(
+					(acc, tag) => {
+						acc[tag] = {
+							message: `@${tag} currently not supported in Typescript`
+						};
+						return acc;
+					},
+					{}
+				),
+				extends: "extends",
+				return: "returns",
+				constructor: "constructor",
+				prop: "property",
+				arg: "param",
+				augments: "extends",
+				description: false,
+				desc: false,
+				inheritdoc: false,
+				class: "constructor"
+			},
+			overrideReplacesDocs: false
+		}
 	},
 	overrides: [
 		{
