2efcc6bf04e9a9bbf1e9a9376cb4f2a2572daa86
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-06-01 21:18:34 +0300

    fix discussions, add more tests

--- test/restrictions.js
+++ test/restrictions.js
@@ -17,7 +17,22 @@ it("should respect RegExp restriction", done => {
 	resolver.resolve({}, fixture, "pck1", {}, (err, result) => {
 		if (!err) throw new Error(`expect error, got ${result}`);
 		err.should.be.instanceof(Error);
-		err.message.should.match(/Resolve restriction \/.*\/ not passed/);
+		done();
+	});
+});
+
+it("should try to find alternative #1", done => {
+	const resolver = ResolverFactory.createResolver({
+		extensions: [".js", ".css"],
+		fileSystem: nodeFileSystem,
+		mainFiles: ["index"],
+		restrictions: [/\.(sass|scss|css)$/]
+	});
+
+	resolver.resolve({}, fixture, "pck1", {}, (err, result) => {
+		if (err) return done(err);
+		if (!result) throw new Error("No result");
+		result.should.equal(path.resolve(fixture, "node_modules/pck1/index.css"));
 		done();
 	});
 });
@@ -32,7 +47,22 @@ it("should respect string restriction", done => {
 	resolver.resolve({}, fixture, "pck2", {}, (err, result) => {
 		if (!err) throw new Error(`expect error, got ${result}`);
 		err.should.be.instanceof(Error);
-		err.message.should.match(/Resolve restriction ".*" not passed/);
+		done();
+	});
+});
+
+it("should try to find alternative #2", done => {
+	const resolver = ResolverFactory.createResolver({
+		extensions: [".js"],
+		fileSystem: nodeFileSystem,
+		mainFields: ["style"],
+		restrictions: [fixture, /\.(sass|scss|css)$/]
+	});
+
+	resolver.resolve({}, fixture, "pck2", {}, (err, result) => {
+		if (err) return done(err);
+		if (!result) throw new Error("No result");
+		result.should.equal(path.resolve(fixture, "node_modules/pck2/index.css"));
 		done();
 	});
 });
