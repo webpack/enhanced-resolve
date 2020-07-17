dabe3eed251d46448ec0bf343da9a3c4a870d932
Tobias Koppers <tobias.koppers@googlemail.com>
2020-07-17 09:45:39 +0200

    fix resolveToContext with fullySpecified

--- test/fullSpecified.js
+++ test/fullSpecified.js
@@ -24,6 +24,7 @@ describe("fullSpecified", function() {
 			"/a/node_modules/package4/a.js": "",
 			"/a/node_modules/package4/b.js": "",
 			"/a/abc.js": "",
+			"/a/dir/index.js": "",
 			"/a/index.js": ""
 		},
 		"/"
@@ -38,11 +39,24 @@ describe("fullSpecified", function() {
 		useSyncFileSystemCalls: true,
 		fileSystem: fileSystem
 	});
+	const contextResolver = ResolverFactory.createResolver({
+		alias: {
+			alias1: "/a/abc",
+			alias2: "/a/"
+		},
+		aliasFields: ["browser"],
+		fullySpecified: true,
+		resolveToContext: true,
+		useSyncFileSystemCalls: true,
+		fileSystem: fileSystem
+	});
 
 	const failingResolves = {
 		"no extensions": "./abc",
+		"no extensions (absolute)": "/a/abc",
 		"no extensions in packages": "package1/file",
-		"no directories": "./",
+		"no directories": ".",
+		"no directories 2": "./",
 		"no directories in packages": "package3/dir",
 		"no extensions in packages 2": "package3/a"
 	};
@@ -50,6 +64,7 @@ describe("fullSpecified", function() {
 	const pkg = "/a/node_modules/package";
 	const successfulResolves = {
 		"fully relative": ["./abc.js", "/a/abc.js"],
+		"fully absolute": ["/a/abc.js", "/a/abc.js"],
 		"fully relative in package": ["package1/file.js", `${pkg}1/file.js`],
 		"extensions in mainFiles": ["package1", `${pkg}1/index.js`],
 		"extensions in mainFields": ["package2", `${pkg}2/a.js`],
@@ -73,6 +88,35 @@ describe("fullSpecified", function() {
 		it(`should resolve ${key} successfully`, () => {
 			try {
 				resolver.resolveSync({}, "/a", request).should.be.eql(expected);
+			} catch (e) {
+				e.message += `\n${e.details}`;
+				throw e;
+			}
+		});
+	}
+
+	const successfulContextResolves = {
+		"current folder": [".", "/a"],
+		"current folder 2": ["./", "/a"],
+		"relative directory": ["./dir", "/a/dir"],
+		"relative directory 2": ["./dir/", "/a/dir"],
+		"relative directory with query and fragment": [
+			"./dir?123#456",
+			"/a/dir?123#456"
+		],
+		"relative directory with query and fragment 2": [
+			"./dir/?123#456",
+			"/a/dir?123#456"
+		],
+		"absolute directory": ["/a/dir", "/a/dir"],
+		"directory in package": ["package3/dir", `${pkg}3/dir`]
+	};
+
+	for (const key of Object.keys(successfulContextResolves)) {
+		const [request, expected] = successfulContextResolves[key];
+		it(`should resolve ${key} successfully to an context`, () => {
+			try {
+				contextResolver.resolveSync({}, "/a", request).should.be.eql(expected);
 			} catch (e) {
 				e.message += `\n${e.details}`;
 				throw e;
