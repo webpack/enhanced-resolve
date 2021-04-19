c84de6743da3aa3f7383475a34823391c4017719
Tobias Koppers <tobias.koppers@googlemail.com>
2021-04-19 10:54:33 +0200

    fix windows test issue

--- test/extensions.js
+++ test/extensions.js
@@ -84,14 +84,14 @@ describe("extensions", function () {
 	it("should default enforceExtension to true when extensions includes an empty string", function (done) {
 		const missingDependencies = new Set();
 		resolver2.resolve({}, fixture, "./foo", { missingDependencies }, () => {
-			missingDependencies.should.not.containEql(fixture + "/foo");
+			missingDependencies.should.not.containEql(path.resolve(fixture, "foo"));
 			done();
 		});
 	});
 	it("should respect enforceExtension when extensions includes an empty string", function (done) {
 		const missingDependencies = new Set();
 		resolver3.resolve({}, fixture, "./foo", { missingDependencies }, () => {
-			missingDependencies.should.containEql(fixture + "/foo");
+			missingDependencies.should.containEql(path.resolve(fixture, "foo"));
 			done();
 		});
 	});
