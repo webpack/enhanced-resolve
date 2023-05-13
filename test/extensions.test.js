b506c7a6a6c25f9cf8ecf2b99e2227bb4916d657
Nitin Kumar <snitin315@gmail.com>
2023-05-13 07:40:21 +0530

    feat: migrate extensions.test.js to jest

--- test/extensions.test.js
+++ test/extensions.test.js
@@ -1,5 +1,3 @@
-require("should");
-
 const path = require("path");
 const fs = require("fs");
 const { ResolverFactory, CachedInputFileSystem } = require("../");
@@ -29,7 +27,7 @@ describe("extensions", function () {
 		resolver.resolve({}, fixture, "./foo", {}, (err, result) => {
 			if (err) return done(err);
 			if (!result) throw new Error("No result");
-			result.should.equal(path.resolve(fixture, "foo.ts"));
+			expect(result).toEqual(path.resolve(fixture, "foo.ts"));
 			done();
 		});
 	});
@@ -37,7 +35,7 @@ describe("extensions", function () {
 		resolver.resolve({}, fixture, "./dir", {}, (err, result) => {
 			if (err) return done(err);
 			if (!result) throw new Error("No result");
-			result.should.equal(path.resolve(fixture, "dir", "index.ts"));
+			expect(result).toEqual(path.resolve(fixture, "dir/index.ts"));
 			done();
 		});
 	});
@@ -45,7 +43,7 @@ describe("extensions", function () {
 		resolver.resolve({}, fixture, ".", {}, (err, result) => {
 			if (err) return done(err);
 			if (!result) throw new Error("No result");
-			result.should.equal(path.resolve(fixture, "index.js"));
+			expect(result).toEqual(path.resolve(fixture, "index.js"));
 			done();
 		});
 	});
@@ -53,7 +51,7 @@ describe("extensions", function () {
 		resolver.resolve({}, fixture, "module", {}, (err, result) => {
 			if (err) return done(err);
 			if (!result) throw new Error("No result");
-			result.should.equal(path.resolve(fixture, "node_modules/module.js"));
+			expect(result).toEqual(path.resolve(fixture, "node_modules/module.js"));
 			done();
 		});
 	});
@@ -61,7 +59,7 @@ describe("extensions", function () {
 		resolver.resolve({}, fixture, "module/", {}, (err, result) => {
 			if (err) return done(err);
 			if (!result) throw new Error("No result");
-			result.should.equal(
+			expect(result).toEqual(
 				path.resolve(fixture, "node_modules/module/index.ts")
 			);
 			done();
@@ -70,28 +68,28 @@ describe("extensions", function () {
 	it("should not resolve to file when request has a trailing slash (relative)", function (done) {
 		resolver.resolve({}, fixture, "./foo.js/", {}, (err, result) => {
 			if (!err) throw new Error("No error");
-			err.should.be.instanceof(Error);
+			expect(err).toBeInstanceOf(Error);
 			done();
 		});
 	});
 	it("should not resolve to file when request has a trailing slash (module)", function (done) {
 		resolver.resolve({}, fixture, "module.js/", {}, (err, result) => {
 			if (!err) throw new Error("No error");
-			err.should.be.instanceof(Error);
+			expect(err).toBeInstanceOf(Error);
 			done();
 		});
 	});
 	it("should default enforceExtension to true when extensions includes an empty string", function (done) {
 		const missingDependencies = new Set();
 		resolver2.resolve({}, fixture, "./foo", { missingDependencies }, () => {
-			missingDependencies.should.not.containEql(path.resolve(fixture, "foo"));
+			expect(missingDependencies).not.toContain(path.resolve(fixture, "foo"));
 			done();
 		});
 	});
 	it("should respect enforceExtension when extensions includes an empty string", function (done) {
 		const missingDependencies = new Set();
 		resolver3.resolve({}, fixture, "./foo", { missingDependencies }, () => {
-			missingDependencies.should.containEql(path.resolve(fixture, "foo"));
+			expect(missingDependencies).toContain(path.resolve(fixture, "foo"));
 			done();
 		});
 	});
