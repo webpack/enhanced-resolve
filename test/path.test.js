503ee0ed6f5738daacf291da5bf3f9944144c846
Danil Vakhrushev <17022974+davakh@users.noreply.github.com>
2024-03-06 04:27:35 +0400

    fix: refactor path tests

--- test/path.test.js
+++ test/path.test.js
@@ -6,6 +6,11 @@ const {
 	normalize
 } = require("../lib/util/path");
 
+// It's enough to use path.sep for tests because the repository has Windows test-runners,
+// but for understanding what to expect, we should know about platform and path-type in tests.
+const isWin32 = process.platform === "win32";
+const currentPathType = isWin32 ? "win32" : "posix";
+
 describe("checkImportsExportsFieldTarget", () => {
 	/**
 	 * @type {string[]}
@@ -35,75 +40,55 @@ describe("checkImportsExportsFieldTarget", () => {
 });
 
 describe("getPath", () => {
-	let pathSepDefault = path.sep;
+	const relativePathType = isWin32 ? "RelativeWin" : "RelativePosix";
 
-	afterAll(() => {
-		path.sep = pathSepDefault;
-	});
-
-	["win32", "posix"].forEach(platform => {
-		const relativePathType =
-			platform === "win32" ? "RelativeWin" : "RelativePosix";
-		const separator = platform === "win32" ? "\\" : "/";
-
-		it(`should resolve PathType.${relativePathType} for paths if path.sep is ${platform} (${separator})`, () => {
-			path.sep = separator;
-
-			expect(getType(".")).toBe(PathType[relativePathType]);
-			expect(getType("..")).toBe(PathType[relativePathType]);
-			expect(getType(`..${path.sep}`)).toBe(PathType[relativePathType]);
-			expect(getType(`..${path.sep}test${path.sep}index.js`)).toBe(
-				PathType[relativePathType]
-			);
-		});
+	it(`should resolve PathType.${relativePathType} for paths if path.sep is ${currentPathType} (${path.sep})`, () => {
+		expect(getType(".")).toBe(PathType[relativePathType]);
+		expect(getType("..")).toBe(PathType[relativePathType]);
+		expect(getType(`..${path.sep}`)).toBe(PathType[relativePathType]);
+		expect(getType(`..${path.sep}test${path.sep}index.js`)).toBe(
+			PathType[relativePathType]
+		);
 	});
 });
 
 describe("normalize", () => {
-	let pathSepDefault = path.sep;
+	it(`should correctly normalize for empty path if path.sep is ${currentPathType} (${path.sep})`, () => {
+		const pathToNormalize = "";
 
-	afterEach(() => {
-		path.sep = pathSepDefault;
+		expect(getType(pathToNormalize)).toBe(PathType.Empty);
+		expect(normalize(pathToNormalize)).toBe("");
 	});
 
-	["win32", "posix"].forEach(platform => {
-		const separator = platform === "win32" ? "\\" : "/";
+	it(`should correctly normalize for relative path if path.sep is ${currentPathType} (${path.sep})`, () => {
+		const pathToNormalize = `..${path.sep}hello${path.sep}world${path.sep}..${path.sep}test.js`;
 
-		it(`should correctly normalize for relative/empty paths if path.sep is ${platform} (${separator})`, () => {
-			path.sep = separator;
-
-			expect(normalize("")).toBe("");
-			expect(
-				normalize(
-					`..${path.sep}hello${path.sep}world${path.sep}..${path.sep}test.js`
-				)
-			).toBe(`..${path.sep}hello${path.sep}test.js`);
-		});
+		expect(getType(pathToNormalize)).toBe(
+			isWin32 ? PathType.RelativeWin : PathType.RelativePosix
+		);
+		expect(normalize(pathToNormalize)).toBe(
+			`..${path.sep}hello${path.sep}test.js`
+		);
 	});
 
-	it("should correctly normalize for PathType.AbsoluteWin", () => {
-		path.sep = "\\";
+	it(`should correctly normalize for absolute path if path.sep is ${currentPathType} (${path.sep})`, () => {
+		const basePath = `${path.sep}hello${path.sep}world${path.sep}..${path.sep}test.js`;
+		const getAbsolutePathPrefixBasedOnPlatform = pathStr =>
+			isWin32 ? `X:${pathStr}` : pathStr;
+		const pathToNormalize = getAbsolutePathPrefixBasedOnPlatform(basePath);
 
-		expect(
-			normalize(
-				`..${path.sep}hello${path.sep}world${path.sep}..${path.sep}test.js`
-			)
-		).toBe(`..${path.sep}hello${path.sep}test.js`);
-	});
-
-	it("should correctly normalize for PathType.AbsolutePosix", () => {
-		path.sep = "/";
-
-		expect(
-			normalize(
-				`..${path.sep}hello${path.sep}world${path.sep}..${path.sep}test.js`
-			)
-		).toBe(`..${path.sep}hello${path.sep}test.js`);
+		expect(getType(pathToNormalize)).toBe(
+			isWin32 ? PathType.AbsoluteWin : PathType.AbsolutePosix
+		);
+		expect(normalize(pathToNormalize)).toBe(
+			getAbsolutePathPrefixBasedOnPlatform(`${path.sep}hello${path.sep}test.js`)
+		);
 	});
 
 	it("should correctly normalize for PathType.Normal", () => {
-		expect(normalize("enhancedResolve/lib/util/../index")).toBe(
-			"enhancedResolve/lib/index"
-		);
+		const pathToNormalize = "enhancedResolve/lib/util/../index";
+
+		expect(getType(pathToNormalize)).toBe(PathType.Normal);
+		expect(normalize(pathToNormalize)).toBe("enhancedResolve/lib/index");
 	});
 });
