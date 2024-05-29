8cd51e7675bb3f7be019ea3ce8f001faeab845f9
alexander.akait <sheo13666q@gmail.com>
2024-05-29 19:07:46 +0300

    test: fix

--- test/SyncAsyncFileSystemDecorator.test.js
+++ test/SyncAsyncFileSystemDecorator.test.js
@@ -42,34 +42,38 @@ describe("SyncAsyncFileSystemDecorator stat", function () {
 describe("SyncAsyncFileSystemDecorator lstat", function () {
 	it("should use options when they're provided", function (done) {
 		const decoratedFs = new SyncAsyncFileSystemDecorator(fs);
-		decoratedFs.lstat(
-			path.join(__dirname, "fixtures", "decorated-fs", "exists.js"),
-			{ bigint: true },
-			function (error, result) {
-				expect(error).toBeNull();
-				expect(result).toHaveProperty("size");
-				expect(result).toHaveProperty("birthtime");
-				expect(
-					typeof (/** @type {import("fs").BigIntStats} */ (result).size)
-				).toEqual("bigint");
-				done();
-			}
-		);
+		if (decoratedFs.lstat) {
+			decoratedFs.lstat(
+				path.join(__dirname, "fixtures", "decorated-fs", "exists.js"),
+				{ bigint: true },
+				function (error, result) {
+					expect(error).toBeNull();
+					expect(result).toHaveProperty("size");
+					expect(result).toHaveProperty("birthtime");
+					expect(
+						typeof (/** @type {import("fs").BigIntStats} */ (result).size)
+					).toEqual("bigint");
+					done();
+				}
+			);
+		}
 	});
 
 	it("should work correctly when no options provided", function (done) {
 		const decoratedFs = new SyncAsyncFileSystemDecorator(fs);
-		decoratedFs.lstat(
-			path.join(__dirname, "fixtures", "decorated-fs", "exists.js"),
-			function (error, result) {
-				expect(error).toBeNull();
-				expect(result).toHaveProperty("size");
-				expect(result).toHaveProperty("birthtime");
-				expect(
-					typeof (/** @type {import("fs").Stats} */ (result).size)
-				).toEqual("number");
-				done();
-			}
-		);
+		if (decoratedFs.lstat) {
+			decoratedFs.lstat(
+				path.join(__dirname, "fixtures", "decorated-fs", "exists.js"),
+				function (error, result) {
+					expect(error).toBeNull();
+					expect(result).toHaveProperty("size");
+					expect(result).toHaveProperty("birthtime");
+					expect(
+						typeof (/** @type {import("fs").Stats} */ (result).size)
+					).toEqual("number");
+					done();
+				}
+			);
+		}
 	});
 });
