3192459743dad06e74847124d0771ef072970059
alexander-akait <sheo13666q@gmail.com>
2025-08-06 14:45:02 +0300

    refactor: allow bundle for web and universal target

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 "use strict";
 
 module.exports = {
+	moduleNameMapper:
+		typeof process.env.ES_MODULE_BUNDLE_TEST !== "undefined"
+			? {
+					"^enhanced-resolve$": "<rootDir>/test/outputs/umd-library/main.js",
+				}
+			: {},
 	prettierPath: require.resolve("prettier-2"),
 	moduleFileExtensions: ["js", "mjs", "cjs", "ts"],
 };
