9d6b8597a597b0309adbf9eddfc790ff2d0dcde6
alexander-akait <sheo13666q@gmail.com>
2025-08-06 14:51:50 +0300

    refactor: fix path to library

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	moduleNameMapper:
 		typeof process.env.ES_MODULE_BUNDLE_TEST !== "undefined"
 			? {
-					"^enhanced-resolve$": "<rootDir>/test/outputs/umd-library/main.js",
+					"^enhanced-resolve$": "<rootDir>/test/outputs/module/main.mjs",
 				}
 			: {},
 	prettierPath: require.resolve("prettier-2"),
