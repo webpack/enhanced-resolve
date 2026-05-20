7d8c24fc2c0eec96374f6a6f0539ac774a0cc5ea
Alexander Akait <4567934+alexander-akait@users.noreply.github.com>
2026-05-20 19:13:37 +0300

    perf: bucket alias options by first char & sort exports keys by priority (#589)

--- types.d.ts
+++ types.d.ts
@@ -222,6 +222,27 @@ declare interface CompiledAliasOption {
 	 */
 	arrayAlias: boolean;
 }
+declare interface CompiledAliasOptions {
+	/**
+	 * declaration-ordered list
+	 */
+	all: CompiledAliasOption[];
+
+	/**
+	 * bucketed by first char code
+	 */
+	byFirstChar: Map<number, CompiledAliasOption[]>;
+
+	/**
+	 * true when an empty-prefix wildcard is present
+	 */
+	hasAnyFirstChar: boolean;
+
+	/**
+	 * true when the bucket fast-path should be used at resolve time
+	 */
+	useBuckets: boolean;
+}
 type Context = KnownContext & Record<any, any>;
 declare interface Dirent<T extends string | Buffer = string> {
 	/**
@@ -1837,7 +1858,7 @@ declare interface TsconfigPathsData {
 	/**
 	 * tsconfig file data
 	 */
-	alias: CompiledAliasOption[];
+	alias: CompiledAliasOptions;
 
 	/**
 	 * tsconfig file data
